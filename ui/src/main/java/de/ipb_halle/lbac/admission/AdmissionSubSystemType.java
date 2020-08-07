905207d0b60e4c7d13d67f45977847387dab9fc3
Fabian Mauz <fmauz@ipb-halle.de>
2020-08-07 10:48:25 +0200

    Updated licence header for all classes in ui project

--- ui/src/main/java/de/ipb_halle/lbac/admission/AdmissionSubSystemType.java
+++ ui/src/main/java/de/ipb_halle/lbac/admission/AdmissionSubSystemType.java
@@ -1,6 +1,6 @@
 /*
- * Leibniz Bioactives Cloud
- * Copyright 2017 Leibniz-Institut f. Pflanzenbiochemie
+ * Cloud Resource & Information Management System (CRIMSy)
+ * Copyright 2020 Leibniz-Institut f. Pflanzenbiochemie
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ import static java.util.stream.Collectors.toMap;
  * LDAP is for all accounts or groups managed by LDAP
  *
  * LBAC_REMOTE is for all accounts or groups managed by a remote instance of 
- * Leibniz Bioactives Cloud
+ * Cloud Resource & Information Management System (CRIMSy)
  */
 
 public enum AdmissionSubSystemType implements Serializable {
--- ui/src/main/java/de/ipb_halle/lbac/service/CloudNodeService.java
+++ ui/src/main/java/de/ipb_halle/lbac/service/CloudNodeService.java
@@ -1,6 +1,6 @@
 /*
- * Leibniz Bioactives Cloud
- * Copyright 2019 Leibniz-Institut f. Pflanzenbiochemie
+ * Cloud Resource & Information Management System (CRIMSy)
+ * Copyright 2020 Leibniz-Institut f. Pflanzenbiochemie
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
