bdf11a53067e80973b936fcb685ad9cdc4b5e823
fmauz <fmauz@ipb-halle.de>
2021-06-07 11:55:35 +0200

    Refactoring of ItemComparator

--- ui/src/main/java/de/ipb_halle/lbac/items/bean/history/ItemComparator.java
+++ ui/src/main/java/de/ipb_halle/lbac/items/bean/history/ItemComparator.java
@@ -1,6 +1,6 @@
 /*
  * Cloud Resource & Information Management System (CRIMSy)
- * Copyright 2020 Leibniz-Institut f. Pflanzenbiochemie
+ * Copyright 2021 Leibniz-Institut f. Pflanzenbiochemie
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,6 +35,10 @@ public class ItemComparator {
 
     private Logger logger;
     private Date mdate;
+    private Item itemOld;
+    private Item itemNew;
+    private boolean differenceFound;
+    private ItemHistory history = new ItemHistory();
 
     public ItemComparator(Date mdate) {
         this.logger = LogManager.getLogger(this.getClass().getName());
@@ -47,10 +51,31 @@ public class ItemComparator {
     }
 
     public ItemHistory compareItems(Item itemOld, Item itemNew, User actor) {
-        boolean differenceFound = false;
-        ItemHistory history = new ItemHistory();
+        this.itemNew = itemNew;
+        this.itemOld = itemOld;
+        this.differenceFound = false;
+
+        initHistoryObject(actor);
+        checkAmount();
+        checkConcentration();
+        checkDescription();
+        checkOwner();
+        checkProject();
+        checkPurity();
+        checkContainer();
+
+        return differenceFound ? history : null;
+
+    }
+
+    private void initHistoryObject(User actor) {
         history.setAction("EDIT");
         history.setActor(actor);
+        history.setItem(itemNew);
+        history.setMdate(mdate);
+    }
+
+    private void checkAmount() {
         BigDecimal amountOld = new BigDecimal(itemOld.getAmount());
         BigDecimal amountNew = new BigDecimal(itemNew.getAmount());
         if (amountOld.compareTo(amountNew) != 0) {
@@ -58,6 +83,9 @@ public class ItemComparator {
             history.setAmountOld(amountOld.doubleValue());
             history.setAmountNew(amountNew.doubleValue());
         }
+    }
+
+    private void checkConcentration() {
         if (!Objects.equals(itemOld.getConcentration(), itemNew.getConcentration())) {
             if (itemOld.getConcentration() == null) {
                 history.setConcentrationNew(itemNew.getConcentration());
@@ -75,38 +103,45 @@ public class ItemComparator {
             }
             differenceFound = true;
         }
+    }
+
+    private void checkDescription() {
         if (!Objects.equals(itemOld.getDescription(), itemNew.getDescription())) {
             differenceFound = true;
             history.setDescriptionNew(itemNew.getDescription());
             history.setDescriptionOld(itemOld.getDescription());
         }
-        history.setItem(itemNew);
-        history.setMdate(mdate);
+    }
+
+    private void checkOwner() {
         if (!itemOld.getOwner().getId().equals(itemNew.getOwner().getId())) {
             differenceFound = true;
             history.setOwnerNew(itemNew.getOwner());
             history.setOwnerOld(itemOld.getOwner());
         }
+    }
+
+    private void checkProject() {
         if (itemNew.getProject().getId() != itemOld.getProject().getId()) {
             differenceFound = true;
             history.setProjectNew(itemNew.getProject());
             history.setProjectOld(itemOld.getProject());
         }
+    }
+
+    private void checkPurity() {
         if (!Objects.equals(itemNew.getPurity(), itemOld.getPurity())) {
             differenceFound = true;
             history.setPurityNew(itemNew.getPurity());
             history.setPurityOld(itemOld.getPurity());
         }
+    }
 
+    private void checkContainer() {
         if (!areContainerEqual(itemOld.getContainer(), itemNew.getContainer())) {
             differenceFound = true;
             history.setParentContainerNew(itemNew.getContainer());
             history.setParentContainerOld(itemOld.getContainer());
-        }
-        if (differenceFound) {
-            return history;
-        } else {
-            return null;
         }
     }
 
@@ -121,4 +156,5 @@ public class ItemComparator {
         }
         return Objects.equals(idOld, idNew);
     }
+
 }
