d5251fd57e2b8d8fee4f5b0c0065d9a9c8de8789
flange-ipb <34936695+flange-ipb@users.noreply.github.com>
2022-01-21 14:58:03 +0100

    - PostgresqlContainerExtension now implements a singleton pattern in order to start the container only once. - load the SQL schema files we use in production - disable strict SQL type validation

--- ui/src/test/java/de/ipb_halle/testcontainers/PostgresqlContainerExtension.java
+++ ui/src/test/java/de/ipb_halle/testcontainers/PostgresqlContainerExtension.java
@@ -18,44 +18,69 @@
 package de.ipb_halle.testcontainers;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.testcontainers.containers.Container.ExecResult;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
 
 /**
- * JUnit5 extension that starts a PostgreSQL docker container once before all
- * tests are invoked and tears it down after all test invocations. The docker
- * container exposed its port 5432 to the host on port 65432 to avoid conflicts
- * with existing PostgreSQL installations.
+ * JUnit5 extension that starts a PostgreSQL docker container <em>once</em>
+ * before all tests are invoked. Testcontainer's <a href=
+ * "https://www.testcontainers.org/test_framework_integration/manual_lifecycle_control/#singleton-containers">singleton
+ * container pattern</a> is employed, thus the container is teared down when the
+ * JVM shuts down. The docker container exposed its port 5432 to the host on
+ * port 65432 to avoid conflicts with existing PostgreSQL installations.
  * <p>
  * Usage: Annotate the test class with
  * {@code @ExtendWith(PostgresqlContainerExtension.class)}
  * 
  * @author flange
  */
-public class PostgresqlContainerExtension implements BeforeAllCallback, AfterAllCallback {
+public class PostgresqlContainerExtension implements BeforeAllCallback {
     private static final String IMAGE_NAME = "ipbhalle/crimsydb:bingo_pg12";
-    private static final String DB_NAME = "lbac";
-    private static final String DB_USER = "lbac";
-    private static final String DB_PASSWORD = "lbac";
 
+    private static final AtomicBoolean FIRST_RUN = new AtomicBoolean(true);
     private PostgreSQLContainer<?> container;
 
-    @SuppressWarnings("resource")
-    @Override
-    public void beforeAll(ExtensionContext context) {
-        DockerImageName customPostgresImage = DockerImageName.parse(IMAGE_NAME).asCompatibleSubstituteFor("postgres");
-        container = new PostgreSQLContainer<>(customPostgresImage).withDatabaseName(DB_NAME).withUsername(DB_USER)
-                .withPassword(DB_PASSWORD);
-        container.setPortBindings(Arrays.asList("65432:5432"));
-        container.start();
-    }
+    private Logger logger = LogManager.getLogger(this.getClass().getName());
 
     @Override
-    public void afterAll(ExtensionContext context) {
-        container.stop();
+    public void beforeAll(ExtensionContext context) throws Exception {
+        if (FIRST_RUN.getAndSet(false)) {
+            startContainer();
+        }
+    }
+
+    @SuppressWarnings("resource")
+    private void startContainer() throws Exception {
+        DockerImageName customPostgresImage = DockerImageName.parse(IMAGE_NAME).asCompatibleSubstituteFor("postgres");
+        container = new PostgreSQLContainer<>(customPostgresImage).withUsername("postgres");
+        container.setPortBindings(Arrays.asList("65432:5432"));
+
+        logger.info("Starting Postgresql container with image " + customPostgresImage.toString());
+        container.start();
+
+        copySchema("schema/00001.sql");
+        applySchema("00001.sql");
+        copySchema("schema/00002.sql");
+        applySchema("00002.sql");
+    }
+
+    private void copySchema(String filename) {
+        logger.info("Copy schema file " + filename + " into container path /");
+        container.copyFileToContainer(MountableFile.forClasspathResource(filename), "/");
+    }
+
+    private void applySchema(String filename) throws Exception {
+        logger.info("Load schema file /" + filename);
+        ExecResult result = container.execInContainer("su", "postgres", "-c psql < /" + filename);
+        logger.info("Stdout: " + result.getStdout());
+        logger.error("Stderr: " + result.getStderr());
     }
 }
