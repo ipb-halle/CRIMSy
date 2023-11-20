429f1e4f2aa2728b2c0d8fd861a62d8b9e8ad3d9
fmauz <fmauz@ipb-halle.de>
2023-11-20 16:45:08 +0100

    Merge commit, part 2

--- tx/src/test/java/de/ipb_halle/tx/text/ParseToolTest.java
+++ tx/src/test/java/de/ipb_halle/tx/text/ParseToolTest.java
@@ -1,15 +1,4 @@
 /*
-<<<<<<< HEAD
- * Cloud Resource & Information Management System (CRIMSy)
- * Copyright 2020 Leibniz-Institut f. Pflanzenbiochemie
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
-=======
  * Text eXtractor
  * Copyright 2023 Leibniz-Institut f. Pflanzenbiochemie
  *
@@ -19,40 +8,11 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
->>>>>>> master
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-<<<<<<< HEAD
- * 
- */
-package de.ipb_halle.tx.text;
-
-import java.io.OutputStream;
-import java.util.Map;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-public class ParseToolTest {
-
-    @Test
-    public void testTool() {
-        ParseTool parseTool = new ParseTool();
-        parseTool.setFilterDefinition(this.getClass().getResourceAsStream("fileParserFilterDefinition.json"));
-        parseTool.setInputStream(this.getClass().getResourceAsStream("ParserTest01.pdf"));
-        parseTool.setOutputStream(OutputStream.nullOutputStream());
-        parseTool.initFilter();
-        parseTool.parse();
-        Map<String, Integer> termVector = (Map<String, Integer>) parseTool.getFilterData().getValue(TermVectorFilter.TERM_VECTOR);
-        assertEquals("single occurence of 'support'", 1L, termVector.get("support").longValue());
-        assertEquals("dual occurence of 'document[s]'", 2L, termVector.get("document").longValue());
-=======
  *
  */
 package de.ipb_halle.tx.text;
@@ -79,6 +39,5 @@ public class ParseToolTest {
         int wordCount = (Integer) parser.getFilterData().getValue(WordDetectorFilter.WORD_COUNT);
 
         Assert.assertEquals("message", 14, wordCount);
->>>>>>> master
     }
 }
