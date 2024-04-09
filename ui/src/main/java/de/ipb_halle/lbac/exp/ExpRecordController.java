efa3c4d9f7e3238784d91dca5c4cc0c2073b46f7
Wittchen <swittche@ipb-halle.de>
2024-04-09 14:16:29 +0200

    fineUploder deleted & bootsfaces entyties

--- ui/src/main/java/de/ipb_halle/lbac/exp/ExpRecordController.java
+++ ui/src/main/java/de/ipb_halle/lbac/exp/ExpRecordController.java
@@ -185,18 +185,6 @@ public abstract class ExpRecordController implements ItemHolder, MaterialHolder 
         }
     }
 
-    /**
-     * Returns JavaScript code to be executed in the onclick event of the "save"
-     * commandButton for this experiment record before executing its AJAX call.
-     * <p>
-     * Clients of this class can overwrite this method to execute their own
-     * JavaScript code. This code may not use the <a href=
-     * "https://showcase.bootsfaces.net/forms/ajax.jsf#basic_usage">BootsFaces-specific
-     * prefixes</a> {@code javascript:} and {@code ajax:}.
-     * 
-     * @return JavaScript code to be executed; the default implementation
-     *         returns an empty string
-     */
     public String getSaveButtonOnClick() {
         return "";
     }
