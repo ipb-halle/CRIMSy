4ddcea1ed5c766bde623bf1ddb7bea0dbfc06095
Hoda Anvari Kazemabad Local <halocal@compchem17.ipb-halle.de>
2025-12-23 14:06:16 +0100

    add script for creating user_sessions table for testing

--- ui/src/test/resources/schema/00001.sql
+++ ui/src/test/resources/schema/00001.sql
@@ -171,6 +171,22 @@ CREATE TABLE nestingpathsets (
     membership_id INTEGER NOT NULL REFERENCES memberships(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
+
+/*
+create user_session class
+*/
+
+CREATE TABLE user_sessions (
+    id          SERIAL NOT NULL PRIMARY KEY,
+    user_id     INTEGER NOT NULL REFERENCES usersGroups (id) ON DELETE CASCADE,
+    token       VARCHAR(255) NOT NULL UNIQUE,
+    last_seen   TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX idx_user_sessions_user_id ON user_sessions(user_id);
+CREATE INDEX idx_user_sessions_last_seen ON user_sessions(last_seen);
+
+
 /* 
  * ToDo: Do some database sanitation for memberships.  
  * This cleanup / sanitation is necessary, when the membership table 
