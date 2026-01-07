36bb175550cdb11e3a4b25c18d199d944496756b
Hoda Anvari Kazemabad Local <halocal@compchem17.ipb-halle.de>
2026-01-07 13:34:12 +0100

    add script of createing user_sessions table

--- docker/db/00004.sql
+++ docker/db/00004.sql
@@ -120,3 +120,18 @@ INSERT INTO reports (context, name, source) VALUES (
     );
 
 COMMIT;
+
+
+/*
+create user_session class
+*/
+
+CREATE TABLE user_sessions (
+    id          SERIAL NOT NULL PRIMARY KEY,
+    user_id     INTEGER NOT NULL REFERENCES usersGroups (id) ON DELETE CASCADE,
+    token       VARCHAR(255) NOT NULL UNIQUE,
+    last_seen   TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX idx_user_sessions_user_id ON user_sessions(user_id);
+CREATE INDEX idx_user_sessions_last_seen ON user_sessions(last_seen);
