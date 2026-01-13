b0009daf17a87595c30f1d8296383e86b9f3b7dd
Hoda Anvari Kazemabad Local <halocal@compchem17.ipb-halle.de>
2026-01-13 11:36:06 +0100

    implement login and logout methods

---
+++ ui/src/main/java/de/ipb_halle/lbac/authentication/AuthApiServiceImpl.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package de.ipb_halle.lbac.authentication;
+
+import de.ipb_halle.api.AuthApiService;
+import de.ipb_halle.lbac.admission.LogInProcess;
+import de.ipb_halle.lbac.admission.MemberEntity;
+import de.ipb_halle.lbac.admission.User;
+import de.ipb_halle.lbac.security.TokenService;
+import de.ipb_halle.model.LoginRequest;
+import de.ipb_halle.model.LoginResponse;
+import de.ipb_halle.model.Logout200Response;
+import de.ipb_halle.model.Logout400Response;
+import de.ipb_halle.model.Logout401Response;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.PersistenceContext;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
+
+/**
+ *
+ * @author halocal
+ */
+public class AuthApiServiceImpl implements AuthApiService {
+
+    @Inject
+    private TokenService tokenService;
+
+    @Inject
+    LogInProcess loginProcess;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public Response logout(SecurityContext securityContext) {
+
+        String token = null;
+        if (securityContext != null && securityContext.getUserPrincipal() != null) {
+            token = securityContext.getUserPrincipal().getName();
+        }
+
+        if (token == null) {
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity(new Logout401Response()
+                            .message("Unathorized: missing token!"))
+                    .build();
+        }
+        try {
+            tokenService.revokeToken(token);
+
+            Logout200Response response = new Logout200Response();
+            response.setMessage("Logout successful");
+            return Response.ok(response).build();
+        } catch (Exception e) {
+            Logout400Response response = new Logout400Response();
+            response.setMessage("Logout faild: " + e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(response).build();
+        }
+    }
+
+    @Override
+    public Response login(LoginRequest loginRequest, SecurityContext securityContext) {
+
+        String login = loginRequest.getLogin();
+        String password = loginRequest.getPassword();
+        User user = loginProcess.tryLogIn(login, password);
+
+        if (user != null) {
+
+//            String token = tokenService.generateToken(user.getLogin());
+            MemberEntity memberEntity;
+
+            try {
+                memberEntity = em.createQuery(
+                        "SELECT m FROM MemberEntity m WHERE m.login = :login", MemberEntity.class)
+                        .setParameter("login", user.getLogin())
+                        .getSingleResult();
+            } catch (NoResultException e) {
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                        .entity("{\"message\":\"User entity not found in database!\"}")
+                        .build();
+            }
+
+            String token = tokenService.generateToken(memberEntity);
+
+            LoginResponse response = new LoginResponse();
+            response.setMessage("Login Succedd!");
+            response.setUsername(user.getLogin());
+            response.setToken(token);
+
+            return Response.status(Response.Status.OK)
+                    .entity(response)
+                    .build();
+        } else {
+
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity("{\"message\":\"Login failed\"}")
+                    .build();
+        }
+    }
+
+}
