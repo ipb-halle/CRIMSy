5ff6ca2cd85d7b8e1d14c660f56de0ec584b974c
Hoda Anvari Kazemabad Local <halocal@compchem17.ipb-halle.de>
2026-01-14 13:23:03 +0100

    remove loginApi and LoginApiService from AppConfiguration

--- ui/src/main/java/de/ipb_halle/lbac/webservice/ApplicationConfig.java
+++ ui/src/main/java/de/ipb_halle/lbac/webservice/ApplicationConfig.java
@@ -17,15 +17,15 @@
  */
 package de.ipb_halle.lbac.webservice;
 
-import de.ipb_halle.api.LoginApi;
-import de.ipb_halle.api.LoginApiService;
+import de.ipb_halle.api.AuthApi;
+import de.ipb_halle.api.AuthApiService;
 import de.ipb_halle.lbac.admission.MembershipWebService;
 import de.ipb_halle.lbac.admission.group.DeactivateGroupWebService;
 import de.ipb_halle.lbac.collections.CollectionWebService;
 import de.ipb_halle.lbac.device.job.PrintWebService;
 import de.ipb_halle.lbac.forum.postings.PostingWebService;
 import de.ipb_halle.lbac.forum.topics.TopicsWebService;
-import de.ipb_halle.lbac.login.LoginEndpoint;
+import de.ipb_halle.lbac.authentication.LoginEndpoint;
 import de.ipb_halle.lbac.search.SearchWebService;
 import de.ipb_halle.lbac.search.document.download.DocumentWebService;
 import de.ipb_halle.lbac.search.wordcloud.WordCloudWebService;
@@ -51,9 +51,9 @@ public class ApplicationConfig extends Application {
                 PrintWebService.class,
                 SimpleRESTPojoExample.class,
                 DocumentWebService.class,
-                LoginApi.class,
-                LoginApiService.class,
+                AuthApiService.class,
+                AuthApi.class,
                 LoginEndpoint.class)
-                );
+        );
     }
 }
