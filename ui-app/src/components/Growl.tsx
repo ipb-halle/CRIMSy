c017cd4449f5ed62838625caaa6addb46c194a49
Hoda Anvari Kazemabad Local <halocal@compchem17.ipb-halle.de>
2026-02-02 16:02:48 +0100

    remove redundant files

--- ui-app/src/components/Growl.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Growl: React.FC = () => {
-    return <div id="growl" className="growlMessages" style={{ position: "fixed", bottom: "1rem", right: "1rem" }} />;
-};
-
-export default Growl;
