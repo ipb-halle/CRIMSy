eb6927fa7141609cf68330c32b376467dbf69bb1
Hoda Anvari Kazemabad Local <halocal@compchem17.ipb-halle.de>
2026-02-16 10:04:15 +0100

    fix(cors): include Access-Control-Allow-Credentials in CORS response headers

--- ui/src/main/java/de/ipb_halle/lbac/security/filter/CorsResponseFilter.java
+++ ui/src/main/java/de/ipb_halle/lbac/security/filter/CorsResponseFilter.java
@@ -10,15 +10,19 @@ public class CorsResponseFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext request,
-                       ContainerResponseContext response) {
+            ContainerResponseContext response) {
 
         response.getHeaders().add(
-            "Access-Control-Allow-Origin", "*");
+                "Access-Control-Allow-Origin", "*");
         response.getHeaders().add(
-            "Access-Control-Allow-Headers",
-            "origin, content-type, accept, authorization");
+                "Access-Control-Allow-Headers",
+                "origin, content-type, accept, authorization");
         response.getHeaders().add(
-            "Access-Control-Allow-Methods",
-            "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+                "Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+
+        response.getHeaders().add(
+                "Access-Control-Allow-Credentials", "true");
+
     }
 }
