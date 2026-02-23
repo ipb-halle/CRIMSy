59cba96ba66dd78420038d6f52823eafa87a9cb7
Hoda Anvari Kazemabad Local <halocal@compchem17.ipb-halle.de>
2026-02-23 10:21:58 +0100

    add more material data in dummyData

--- ui-app/src/data/dummyData.ts
+++ ui-app/src/data/dummyData.ts
@@ -6,7 +6,10 @@ export const dummyData = {
   materials: [
     { name: "Material 1", type: "Structure", project: "Project A", user: "Alice", container: "Box", storageClass: "Cold", hazard: "Low", deactivationStatus: "Active" },
     { name: "Material 2", type: "Sequence", project: "Project B", user: "Bob", container: "Shelf", storageClass: "Room", hazard: "Medium", deactivationStatus: "Inactive" },
+    { name: "Material 3", type: "Biomaterial", project: "Project B", user: "Alice", container: "Cabinet", storageClass: "Frezer", hazard: "High", deactivationStatus: "Inactive" },
+    { name: "Material 4", type: "Composition", project: "Project A", user: "Bob", container: "Cabinet", storageClass: "Room", hazard: "Low", deactivationStatus: "Active" },
   ],
+
   materialTypes: ["Structure", "Sequence", "Biomaterial", "Composition"],
   containers: ["Box", "Shelf", "Cabinet"],
   storageClasses: ["Cold", "Room", "Freezer"],
