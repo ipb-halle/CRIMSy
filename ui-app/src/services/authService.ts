3102f4ec7855d3cfa123fd550094d7711f96cd5b
Hoda Anvari Kazemabad Local <halocal@compchem17.ipb-halle.de>
2026-03-17 09:05:09 +0100

    add alert msg for logoutAPI

--- ui-app/src/services/authService.ts
+++ ui-app/src/services/authService.ts
@@ -1,6 +1,9 @@
 import { AuthApi } from "../adapters/api";
 import { Configuration } from "../adapters/api";
-import { AuthToken, LoginRequest, User } from "../adapters/api/models";
+import { AuthToken, LoginRequest, AuthUser, LogoutResponse } from "../adapters/api/models";
+
+
+let BASE = "https://compchem17.ipb-halle.de/ui/rest/auth/logout";
 
 export const loginAPI = async (
   loginRequest: LoginRequest
@@ -15,42 +18,82 @@ export const loginAPI = async (
   return response;
 };
 
-export const logoutAPI = async (
-  token: string
-): Promise<void> => {
+export const logoutAPII = async (token: string): Promise<LogoutResponse> => {
+  if (!token) throw new Error("No token found");
+
   const config = new Configuration({
-    accessToken: async () => token
+    accessToken: async (_name?: string, _scopes?: string[]) => token
   });
+
   const api = new AuthApi(config);
 
-  await api.logout();
+  try {
+    const res = await api.logout();
+    if (!res.message) {
+      throw new Error(`Logout failed: ${res.message}`);
+    }
+    return res;
+
+  } catch (error) {
+    throw new Error(`Logout failed: ${error}`);
+  }
+};
+
+
+export const logoutAPI = async (token: string): Promise<LogoutResponse> => {
+  if (!token) throw new Error("No token found");
+  const res = await fetch(`${BASE}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+  });
+
+  if (!res.ok) {
+    const errorText = await res.text();
+    throw new Error(`Logout failed: ${res.status} ${errorText}`);
+  }
+
+  const responseBody = await res.text();
+  if (!responseBody) {
+    throw new Error("Logout response is empty");
+  }
+
+  const data = JSON.parse(responseBody);
+  alert("Logged out Successfully!\n Click to continue!");
+  return data;
 };
 
 export const checkSessionAPI = async (
   token: string
-): Promise<User> => {
+): Promise<AuthUser> => {
   const api = new AuthApi(
     new Configuration({
       accessToken: async () => token
     })
   );
 
-  return await api.authMeGet();
+  return await api.getCurrentUser();
 };
 
 export const fetchRoleAPI = async (
   token: string
-): Promise<User> => {
+): Promise<AuthUser> => {
   const config = new Configuration({
     accessToken: async () => token
   });
 
   const roleApi = new AuthApi(config);
 
-  const roleResponse = await roleApi.authMeGet();
-  return roleResponse;
+  const authUser = await roleApi.getCurrentUser();
+  return authUser;
 };
 
+
+function expireSession(arg0: string) {
+  throw new Error("Function not implemented.");
+}
 /*export const fetchUsersAPI = async (
   token: string,
   page: number, pageSize: number
